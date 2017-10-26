34716c7bd055b0012eb4f0fc100174f261d37bd5
Chanho Kong <chris.kong@doomoolmori.com>
2017-10-26 21:12:14 +0900

    - 테스트 스크립트 수정

--- ansible-npm-circleci/test.sh
+++ ansible-npm-circleci/test.sh
@@ -1,6 +1,7 @@
-REQ=( make git )
+#!/bin/bash
+REQ="make git"
 
-for i in "${REQ[@]}"
+for i in $REQ
 do
   echo 'test' $i 'exists'
   command -v $i >/dev/null 2>&1 || { echo >&2 "I require $i but it's not installed.  Aborting."; exit 1; }
