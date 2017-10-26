53306f40162b9e0194aa33803d0c1ac3b4957926
Chanho Kong <chris.kong@doomoolmori.com>
2017-10-26 20:58:09 +0900

    - Automated repository tests 를 위해 docker compose 와 테스트 파일 추가

---
+++ ansible-npm-circleci/test.sh
@@ -0,0 +1,8 @@
+REQ=( make git )
+
+for i in "${REQ[@]}"
+do
+  echo 'test' $i 'exists'
+  command -v $i >/dev/null 2>&1 || { echo >&2 "I require $i but it's not installed.  Aborting."; exit 1; }
+  echo 'ok'
+done
